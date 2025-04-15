33ce810386e4ae2a96826364236f8554461573ce
cbbbendan <1276969254@qq.com>
2025-04-15 10:47:30 +0800

    change in index.vue about css type

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -292,7 +292,7 @@
 		height: 100%;
 		box-sizing: border-box;
 		border-radius: 10px;
-		border: 1px solid #cad1c3;
+
 		width: 100%;
 
 		.anoun1 {
@@ -422,7 +422,7 @@
 		}
 
 		.left-upper {
-			margin: 10rpx;
+			margin: 15rpx;
 			// text-align: center;
 			width: 45%;
 			height: 110rpx;
@@ -433,7 +433,7 @@
 
 		.right-upper {
 			position: relative;
-			margin: 10rpx;
+			margin: 15rpx;
 			width: 45%;
 			height: 110rpx;
 			border-radius: 10px;
