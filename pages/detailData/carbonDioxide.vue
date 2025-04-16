65cdf9bca735c6a6a9049458d4fbaaa0fbf3aa48
Paitalyi <maopengyang@foxmail.com>
2025-04-16 17:12:14 +0800

    Fix

--- pages/detailData/carbonDioxide.vue
+++ pages/detailData/carbonDioxide.vue
@@ -1,106 +1,122 @@
 <template>
-  <div ref="myChart"></div>
+	<div ref="myChart"></div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import * as echarts from 'echarts';
-import axios from 'axios';
+	import {
+		onMounted,
+		ref
+	} from 'vue';
+	import * as echarts from 'echarts';
+	import axios from 'axios';
+	import { useFetch } from '../../composables/useFetch';
 
-const myChart = ref(null);
-// 请替换为实际的 CO₂ 数据接口地址
-const apiUrl = 'http://192.168.20.237:8080/getCo2Top10';
+	const myChart = ref(null);
+	// 请替换为实际的 CO₂ 数据接口地址
+	const { baseUrl } = useFetch();
+	const relativeUrl = '/getCo2Top10';
+	const apiUrl = `${baseUrl}${relativeUrl}`;
 
-onMounted(async () => {
-  const chartDom = myChart.value;
-  const echartInstance = echarts.init(chartDom);
+	onMounted(async () => {
+		const chartDom = myChart.value;
+		const echartInstance = echarts.init(chartDom);
 
-  try {
-    const response = await axios.get(apiUrl);
-    const rawData = response.data;
+		try {
+			const response = await axios.get(apiUrl);
+			const rawData = response.data;
 
-    const processedData = rawData.map(item => {
-      const [datePart, timePart] = item.createdAt.split(' ');
-      const [, month, day] = datePart.split('-');
-      const hour = timePart.split(':')[0];
-      const monthDayHour = `${month}-${day} ${hour}时`;
-      return {
-        monthDayHour,
-        co2: item.co2
-      };
-    });
+			const processedData = rawData.map(item => {
+				const [datePart, timePart] = item.createdAt.split(' ');
+				const [, month, day] = datePart.split('-');
+				const hour = timePart.split(':')[0];
+				const monthDayHour = `${month}-${day} ${hour}时`;
+				return {
+					monthDayHour,
+					co2: item.co2
+				};
+			});
 
-    // 按时间排序数据
-    processedData.sort((a, b) => {
-      const timeA = new Date(`2025-${a.monthDayHour.replace('时', '')}`).getTime();
-      const timeB = new Date(`2025-${b.monthDayHour.replace('时', '')}`).getTime();
-      return timeA - timeB;
-    });
+			// 按时间排序数据
+			processedData.sort((a, b) => {
+				const timeA = new Date(`2025-${a.monthDayHour.replace('时', '')}`).getTime();
+				const timeB = new Date(`2025-${b.monthDayHour.replace('时', '')}`).getTime();
+				return timeA - timeB;
+			});
 
-    const xAxisData = processedData.map(item => item.monthDayHour);
-    const seriesData = processedData.map(item => item.co2);
+			const xAxisData = processedData.map(item => item.monthDayHour);
+			const seriesData = processedData.map(item => item.co2);
 
-    const option = {
-      title: { text: '详细 CO₂ 信息' },
-      tooltip: {
-        trigger: 'axis',
-        triggerOn: 'touchstart',
-        formatter: (params) => `${params.name}<br>CO₂ 含量: ${params.value}`
-      },
-      legend: { data: ['CO₂ 含量'] },
-      grid: {
-        left: '3%',
-        right: '8%',
-        bottom: '3%',
-        containLabel: true
-      },
-      toolbox: { feature: { saveAsImage: {} } },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: xAxisData,
-        axisLabel: {
-          textStyle: {
-            fontSize: 10
-          },
-          rotate: 45
-        }
-      },
-      yAxis: {
-        type: 'value',
-        axisLabel: { formatter: '{value}' }
-      },
-      series: [
-        {
-          name: 'CO₂ 含量',
-          type: 'line',
-          symbol: 'circle',
-          symbolSize: 12,
-          emphasis: { symbolSize: 16 },
-          data: seriesData
-        }
-      ]
-    };
+			const option = {
+				title: {
+					text: '详细 CO₂ 信息'
+				},
+				tooltip: {
+					trigger: 'axis',
+					triggerOn: 'touchstart',
+					formatter: (params) => `${params.name}<br>CO₂ 含量: ${params.value}`
+				},
+				legend: {
+					data: ['CO₂ 含量']
+				},
+				grid: {
+					left: '3%',
+					right: '8%',
+					bottom: '3%',
+					containLabel: true
+				},
+				toolbox: {
+					feature: {
+						saveAsImage: {}
+					}
+				},
+				xAxis: {
+					type: 'category',
+					boundaryGap: false,
+					data: xAxisData,
+					axisLabel: {
+						textStyle: {
+							fontSize: 10
+						},
+						rotate: 45
+					}
+				},
+				yAxis: {
+					type: 'value',
+					axisLabel: {
+						formatter: '{value}'
+					}
+				},
+				series: [{
+					name: 'CO₂ 含量',
+					type: 'line',
+					symbol: 'circle',
+					symbolSize: 12,
+					emphasis: {
+						symbolSize: 16
+					},
+					data: seriesData
+				}]
+			};
 
-    option && echartInstance.setOption(option);
+			option && echartInstance.setOption(option);
 
-    echartInstance.on('click', (params) => {
-      console.log('点击时间:', params.name, 'CO₂ 含量:', params.value);
-    });
+			echartInstance.on('click', (params) => {
+				console.log('点击时间:', params.name, 'CO₂ 含量:', params.value);
+			});
 
-    echartInstance.on('longpress', (params) => {
-      console.log('长按时间:', params.name, 'CO₂ 含量:', params.value);
-    });
-  } catch (error) {
-    console.error('数据获取失败:', error);
-  }
-});
+			echartInstance.on('longpress', (params) => {
+				console.log('长按时间:', params.name, 'CO₂ 含量:', params.value);
+			});
+		} catch (error) {
+			console.error('数据获取失败:', error);
+		}
+	});
 </script>
 
 <style scoped>
-div {
-  width: 100%;
-  height: 400px;
-  touch-action: none;
-}
-</style>    
+	div {
+		width: 100%;
+		height: 400px;
+		touch-action: none;
+	}
+</style>
--- pages/detailData/temDetail.vue
+++ pages/detailData/temDetail.vue
@@ -1,102 +1,125 @@
 <template>
-  <div ref="myChart"></div>
+	<div ref="myChart"></div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import * as echarts from 'echarts';
-import axios from 'axios';
+	import {
+		onMounted,
+		ref
+	} from 'vue';
+	import * as echarts from 'echarts';
+	import axios from 'axios';
+	import { useFetch } from '../../composables/useFetch';
 
-const myChart = ref(null);
-// 请将此替换为实际的后端接口地址
-const apiUrl = 'http://192.168.20.237:8080/getTempTop10';
+	const myChart = ref(null);
+	// 请将此替换为实际的后端接口地址
+	const { baseUrl } = useFetch();
+	const relativeUrl = '/getTempTop10';
+	const apiUrl = `${baseUrl}${relativeUrl}`;
 
-onMounted(async () => {
-  const chartDom = myChart.value;
-  const echartInstance = echarts.init(chartDom);
+	onMounted(async () => {
+		const chartDom = myChart.value;
+		const echartInstance = echarts.init(chartDom);
 
-  try {
-    const response = await axios.get(apiUrl);
-    const rawData = response.data;
+		try {
+			const response = await axios.get(apiUrl);
+			const rawData = response.data;
 
-    const processedData = rawData.map(item => {
-      const [datePart, timePart] = item.createdAt.split(' ');
-      const [, month, day] = datePart.split('-');
-      const hour = timePart.split(':')[0];
-      const monthDayHour = `${month}-${day} ${hour}时`;
-      return {
-        monthDayHour,
-        temperature: item.temperature
-      };
-    });
+			const processedData = rawData.map(item => {
+				const [datePart, timePart] = item.createdAt.split(' ');
+				const [, month, day] = datePart.split('-');
+				const hour = timePart.split(':')[0];
+				const monthDayHour = `${month}-${day} ${hour}时`;
+				return {
+					monthDayHour,
+					temperature: item.temperature
+				};
+			});
 
-   
-    const xAxisData = processedData.map(item => item.monthDayHour);
-    const seriesData = processedData.map(item => item.temperature);
-	xAxisData.reverse();
-	seriesData.reverse();
-    const option = {
-      title: { text: '详细温度信息' },
-      tooltip: {
-        trigger: 'axis',
-        triggerOn: 'touchstart',
-        formatter: (params) => `${params.name}<br>温度: ${params.value}°C`
-      },
-      legend: { data: ['Temperature'] },
-      grid: { left: '3%', right: '4%', bottom: '3%', containLabel: true },
-      toolbox: { feature: { saveAsImage: {} } },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: xAxisData,
-		axisLabel:{
-			textStyle:{fontSize: 12},
-			rotate: 45
-		},
-		
-      },
-      yAxis: {
-        type: 'value',
-        axisLabel: { formatter: '{value}°C' }
-      },
-      series: [
-        {
-          name: 'Temperature',
-          type: 'line',
-          symbol: 'circle',
-          symbolSize: 12,
-          emphasis: { symbolSize: 16 },
-          data: seriesData
-        }
-      ],
-	  grid: {
-	      left: '3%',
-	      right: '8%', // 增大右侧边距，可根据实际情况调整
-	      bottom: '3%',
-	      containLabel: true
-	  }
-    };
 
-    option && echartInstance.setOption(option);
+			const xAxisData = processedData.map(item => item.monthDayHour);
+			const seriesData = processedData.map(item => item.temperature);
+			xAxisData.reverse();
+			seriesData.reverse();
+			const option = {
+				title: {
+					text: '详细温度信息'
+				},
+				tooltip: {
+					trigger: 'axis',
+					triggerOn: 'touchstart',
+					formatter: (params) => `${params.name}<br>温度: ${params.value}°C`
+				},
+				legend: {
+					data: ['Temperature']
+				},
+				grid: {
+					left: '3%',
+					right: '4%',
+					bottom: '3%',
+					containLabel: true
+				},
+				toolbox: {
+					feature: {
+						saveAsImage: {}
+					}
+				},
+				xAxis: {
+					type: 'category',
+					boundaryGap: false,
+					data: xAxisData,
+					axisLabel: {
+						textStyle: {
+							fontSize: 12
+						},
+						rotate: 45
+					},
 
-    echartInstance.on('click', (params) => {
-      console.log('点击时间:', params.name, '温度:', params.value);
-    });
+				},
+				yAxis: {
+					type: 'value',
+					axisLabel: {
+						formatter: '{value}°C'
+					}
+				},
+				series: [{
+					name: 'Temperature',
+					type: 'line',
+					symbol: 'circle',
+					symbolSize: 12,
+					emphasis: {
+						symbolSize: 16
+					},
+					data: seriesData
+				}],
+				grid: {
+					left: '3%',
+					right: '8%', // 增大右侧边距，可根据实际情况调整
+					bottom: '3%',
+					containLabel: true
+				}
+			};
 
-    echartInstance.on('longpress', (params) => {
-      console.log('长按时间:', params.name, '温度:', params.value);
-    });
-  } catch (error) {
-    console.error('数据获取失败:', error);
-  }
-});
+			option && echartInstance.setOption(option);
+
+			echartInstance.on('click', (params) => {
+				console.log('点击时间:', params.name, '温度:', params.value);
+			});
+
+			echartInstance.on('longpress', (params) => {
+				console.log('长按时间:', params.name, '温度:', params.value);
+			});
+		} catch (error) {
+			console.error('数据获取失败:', error);
+		}
+	});
 </script>
 
 <style scoped>
-div {
-  width: 95%;
-  height: 400px;
-  touch-action: none;
-  
-}
-</style>    
+	div {
+		width: 95%;
+		height: 400px;
+		touch-action: none;
+
+	}
+</style>
